aee550db06e7f78e8f233b43622269a44a6eeb3d
David Kant <david.kant@gmail.com>
2018-10-09 08:03:55 -0700

    add ypitch pitch estmation using yin

--- mai/listen.py
+++ mai/listen.py
@@ -6,6 +6,23 @@ def spectrogram(y, **kwargs):
     """Compute db-scale magnitude spectrum."""
     return librosa.amplitude_to_db(np.abs(librosa.stft(y, **kwargs)), ref=np.max)
 
+def ypitch(y, frame_length, hop_length):
+    """Estimate pitch using the yin algorithm."""
+
+    # use the yin pitch tracking algorithm
+    pitch_tracker = essentia.standard.PitchYin()
+
+    pitches = []
+    confidences = []
+
+    # loop through frames and analyze pitch
+    for frame in essentia.standard.FrameGenerator(y, frameSize=frame_length, hopSize=hop_length, startFromZero=True):
+        pitch, confidence = pitch_tracker(frame)
+        pitches.append(pitch)
+        confidences.append(confidence) 
+
+    return pitches, confidences
+
 def spectral_features(filename, frameSize=2048, hopSize=1024, sr=44100):
     """Extract basic spectral features."""
 
