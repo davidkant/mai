15094a4cc8372a5a60b838a6383e5795724f3adf
David Kant <david.kant@gmail.com>
2018-10-12 07:48:30 -0700

    write and play mp3 files

---
+++ mai/io.py
@@ -0,0 +1,19 @@
+import librosa
+import os 
+
+
+def play_audio(x, mp3=True, sr=22050):
+    """Write audio to mp3 and return Ipython widget."""
+    # write temp wav file
+    librosa.output.write_wav('temp.wav', x, sr=sr)
+    # convert to mp3 using lame
+    !lame temp.wav temp.mp3 --quiet
+    # return ipython audio widget
+    return IPython.display.Audio('temp.mp3')
+
+def write_mp3(x, filename='temp.mp3', sr=22050):
+    """Write audio to mp3."""
+    # write temp wav file
+    librosa.output.write_wav('temp.wav', x, sr=sr)
+    # convert wav file to mp3 using lame
+    os.system("lame temp.wav {} --quiet".format(filename))
