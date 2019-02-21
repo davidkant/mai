7e3f830ef822c488c7edc918e7b63fed5a6921b8
David Kant <david.kant@gmail.com>
2019-02-21 11:30:29 -0800

    Added fm function as convenience

--- mai/synths.py
+++ mai/synths.py
@@ -75,6 +75,10 @@ class FM1():
         dur = int(self.sr * self.attack) + int(self.sr * self.release)
         return np.array([self.next() for i in range(dur-1)])
 
+def fm(carrier=900, modulator=300, index1=5, index2=0, attack=0.01, release=1, sr=44100):
+    """A convenience to render FM1() as function call."""
+    return FM(carrier=carrier, modulator=modulator, index1=index2, index2=index2, attack=attack, release=release, sr=sr).render()
+
 class FeedbackFM1():
     """A coupled feedback FM synth with a few control parameters."""
 
